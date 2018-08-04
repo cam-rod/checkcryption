c7a8a61e6f50c001ee99e29a5b1f426c2888ed8b
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-04 19:59:31 -0400

    initialized crypter function

--- crypter.py
+++ crypter.py
@@ -1,13 +1,18 @@
-"""This module handles the encryption of decryption of data.
+"""This module handles the encryption of decryption of data."""
 
-Arguments:
-  - text: in the form of plaintext (to be encrypted) or encrypted data (to be decrypted), depending on what is requested
-  - hashed_password: hashed (scrambled) version of the password, used to generate the encryption key
+def crypter(password, text, crypt):
+  """This function temporarily generates the encryption key, calls the correct function, and then returns the text.
 
-Other variables:
-  - encryption_key: the key which directly instructs the program how to encrypt the data
+  Arguments:
+    - password: hashed (scrambled) version of the password, used to generate the encryption key
+    - text: in the form of plaintext (to be encrypted) or encrypted data (to be decrypted), depending on what is requested
+    - crypt: what should happen to the text (encryption or decryption)
 
-Returns:
-  - crypted_text: the encrypted (after encryption) or plaintext data (after decryption)
-"""
+  Other variables:
+    - encryption_key: the key which directly instructs the program how to encrypt the data
 
+  Returns:
+    - the encrypted (after encryption) or plaintext data (after decryption)
+  """
+  
+    
