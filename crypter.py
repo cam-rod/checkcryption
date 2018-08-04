cfbc349ce54ec6c8c85dabf1aaaa210ad05620e6
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-04 19:45:48 -0400

    moved plaintext password processing to accounts.py

--- crypter.py
+++ crypter.py
@@ -2,10 +2,9 @@
 
 Arguments:
   - text: in the form of plaintext (to be encrypted) or encrypted data (to be decrypted), depending on what is requested
-  - password: the user password which is converted into the encryption key
+  - hashed_password: hashed (scrambled) version of the password, used to generate the encryption key
 
 Other variables:
-  - hashed_password: hashed (scrambled) version of the password, used to generate the encryption key
   - encryption_key: the key which directly instructs the program how to encrypt the data
 
 Returns:
