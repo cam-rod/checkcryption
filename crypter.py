66234cad34dde29df890949c8aae66d74dcd610a
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-05 16:38:49 -0400

    implememnted key gen 1/3

--- crypter.py
+++ crypter.py
@@ -29,8 +29,47 @@ class Crypter:
   def encryption_key(self, password):
     "This function generates the encryption key."
     if self.password != None:
-      fragments = [] # Individual characters in encryption key
-      for i in self.password[0:4]
-        ]: # Each character of the password
+      fragments = [] # Individual numbers in encryption key
+      print("Generating encryption key...")
+      for i in self.password[0:4]: # Each character of the password
         if i == ('N' or 'r' or '(' or 'D' or 'c' or '.' or 'V'):
-          fragments.append('0')
+          fragments.append(0)
+        elif i == ('Q' or 'n' or 'J' or 'e' or 'Z' or 'a' or 'k'):
+          fragments.append(1)
+        elif i == ('i' or '7' or '*' or 'z' or 'H' or 'u' or 'y'):
+          fragments.append(2)
+        elif i == ('S' or 'F' or '[' or 'j' or 'h' or 'R' or '3' or 'x'):
+          fragments.append(3)
+        elif i == ('p' or 'Y' or 'o' or '#' or '5' or 'w' or 'U'):
+          fragments.append(4)
+        elif i == ('b' or 'T' or '0' or 'g' or 'K' or 'A' or '2'):
+          fragments.append(5)
+        elif i == ('@' or 'X' or 'v' or 'I' or '8' or 's' or '1'):
+          fragments.append(6)
+        elif i == (')' or 'O' or 'd' or 'l' or 'L' or 'q' or 'C'):
+          fragments.append(7)
+        elif i == ('/' or ',' or 'W' or '4' or 't' or 'P' or 'B' or '9'):
+          fragments.append(8)
+        else:
+          fragments.append(9)
+      for i in self.password[4:8]: # Each character of the password
+        if i == ():
+          fragments.append(0)
+        elif i == ():
+          fragments.append(1)
+        elif i == ():
+          fragments.append(2)
+        elif i == ():
+          fragments.append(3)
+        elif i == ():
+          fragments.append(4)
+        elif i == ():
+          fragments.append(5)
+        elif i == ():
+          fragments.append(6)
+        elif i == ():
+          fragments.append(7)
+        elif i == ():
+          fragments.append(8)
+        else:
+          fragments.append(9)
