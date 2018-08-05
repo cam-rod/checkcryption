7afac3b61ca22dbfd1572bd14d76d32a9eb466e3
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-04 20:41:50 -0400

    created encryption_key property in crypter

--- crypter.py
+++ crypter.py
@@ -1,7 +1,7 @@
 """This module handles the encryption of decryption of data."""
 
-def crypter(password, text, crypt):
-  """This function temporarily generates the encryption key, calls the correct function, and then returns the text.
+class Crypter:
+  """This class temporarily generates the encryption key, calls the correct function, and then returns the text.
 
   Arguments:
     - password: hashed (scrambled) version of the password, used to generate the encryption key
@@ -9,10 +9,24 @@ def crypter(password, text, crypt):
     - crypt: what should happen to the text (encryption or decryption)
 
   Other variables:
-    - encryption_key: the key which directly instructs the program how to encrypt the data
+    - encryption_key: the key which directly instructs the program how to encrypt the data (hashed and salted)
 
   Returns:
     - the encrypted (after encryption) or plaintext data (after decryption)
   """
+
+  def __init__(self, password, text, crypt):
+    """Initializes the variables."""
+    self.password = password
+    self.text = text
+    self.crypt = crypt
   
-    
+  @property
+  def encryption_key(self): # Stored as property to allow other areas to allow encryter decryptor to call key
+    return self._encryption_key
+  
+  @encryption_key.setter
+  def encryption_key(self, password):
+    "This function generates the encryption key."
+    if self.password != None:
+      for i in self.password: # Each character of the password
