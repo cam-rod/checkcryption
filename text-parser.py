a2669f20ba3021afc0e93ed4f5e76fc2ec1a3b01
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-08 18:47:28 -0400

    added goals of text-parser.py

--- text-parser.py
+++ text-parser.py
@@ -1,4 +1,8 @@
 """This module interprets text from various sources.
 
 Functions:
-  - Recieves input for and verifies the password for the crypter classmethod
+  - Recieves input for and verifies a valid password for signing in.
+  - Reads text from regular files or direct entry and converts to binary.
+  - Write's encrypted text to files.
+  - Reads and returns matching section from users.txt.
+  - Writes usernames to users.txt."""
