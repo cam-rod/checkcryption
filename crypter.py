0b2bbb5695a3cd1f580ef69547cf5769951dd87e
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-07 20:52:12 -0400

    now using regex in key gen

--- crypter.py
+++ crypter.py
@@ -1,5 +1,7 @@
 """This module handles the encryption of decryption of data."""
 
+import re
+
 class Crypter:
   """This class temporarily generates the encryption key, calls the correct function, and then returns the text.
 
@@ -33,65 +35,65 @@ class Crypter:
       salt = [] # Salted numbers to be added to e
       print("Generating encryption key...")
       for i in self.password[0:4]: # 1st-4th characters of the password
-        if i == ('N' or 'r' or '(' or 'D' or 'c' or '.' or 'V'):
+        if re.match(r'[Nr\(Dc\.V]', i) != None:
           e.append(0)
-        elif i == ('Q' or 'n' or 'J' or 'e' or 'Z' or 'a' or 'k'):
+        elif re.match(r'[QnJeZak]', i) != None:
           e.append(1)
-        elif i == ('i' or '7' or '*' or 'z' or 'H' or 'u' or 'y'):
+        elif re.match(r'[i7\*zHuy]', i) != None:
           e.append(2)
-        elif i == ('S' or 'F' or '[' or 'j' or 'h' or 'R' or '3' or 'x'):
+        elif re.match(r'[SF\[jhR3x]', i) != None:
           e.append(3)
-        elif i == ('p' or 'Y' or 'o' or '#' or '5' or 'w' or 'U'):
+        elif re.match(r'[pYo#5wU]', i) != None:
           e.append(4)
-        elif i == ('b' or 'T' or '0' or 'g' or 'K' or 'A' or '2'):
+        elif re.match(r'[bT0gKA2]', i) != None:
           e.append(5)
-        elif i == ('@' or 'X' or 'v' or 'I' or '8' or 's' or '1'):
+        elif re.match(r'[@XvI8s1]', i) != None:
           e.append(6)
-        elif i == (')' or 'O' or 'd' or 'l' or 'L' or 'q' or 'C'):
+        elif re.match(r'[\)OdlLqC]', i) != None:
           e.append(7)
-        elif i == ('/' or ',' or 'W' or '4' or 't' or 'P' or 'B' or '9'):
+        elif re.match(r'[/,W4tPB9]', i) != None:
           e.append(8)
         else:
           e.append(9)
       for i in self.password[4:8]: # 5th-8th characters of the password
-        if i == ('A' or 'i' or 'y' or '5' or 'o' or 'X' or 't'):
+        if re.match(r'[Aiy5oXt]', i) != None:
           e.append(0)
-        elif i == ('.' or 'c' or 'e' or 'D' or 'k' or 'U' or 'Q' or '1'):
+        elif re.match(r'[\.ceDkUQ1]', i) != None:
           e.append(1)
-        elif i == ('H' or 'v' or 'a' or 'N' or 'x' or 'J' or '@'):
+        elif re.match(r'[HvaNxJ@]', i) != None:
           e.append(2)
-        elif i == ('q' or 'B' or ']' or 'g' or '*' or 'L' or '6'):
+        elif re.match(r'[qB\]g\*L6]', i) != None:
           e.append(3)
-        elif i == ('G' or 's' or '0' or 'Z' or 'O' or 'd' or 'b' or 'p'):
+        elif re.match(r'[Gs0ZOdbp]', i) != None:
           e.append(4)
-        elif i == ('l' or 'w' or ',' or '9' or '2' or 'n' or 'M'):
+        elif re.match(r'[lw,92nM]', i) != None:
           e.append(5)
-        elif i == ('E' or 'T' or 'f' or '/' or 'z' or '7' or '(' or 'I'):
+        elif re.match(r'[ETf/z7\(I]', i) != None:
           e.append(6)
-        elif i == ('u' or 'C' or 'F' or 'S' or 'P' or 'Y' or 'h'):
+        elif re.match(r'[uCFSPYh]', i) != None:
           e.append(7)
-        elif i == ('3' or 'R' or '#' or '!' or '8' or 'K' or 'm'):
+        elif re.match(r'[3R#!8Km]', i) != None:
           e.append(8)
         else:
           e.append(9)
       for i in self.password[8:12]: # 9th-12th characters of the password
-        if i == ('F' or 'w' or '9' or '@' or '#' or 'o' or 'S'):
+        if re.match(r'[Fw9@#oS]', i) != None:
           e.append(0)
-        elif i == ('P' or '4' or 'h' or 'X' or 'q' or 'a' or 'J'):
+        elif re.match(r'[P4hXqaJ]', i) != None:
           e.append(1)
-        elif i == (']' or 'C' or 'g' or 'H' or 'z' or 'B' or 'R'):
+        elif re.match(r'[\]CgHzBR]', i) != None:
           e.append(2)
-        elif i == ('7' or ',' or 'e' or 'Q' or 't' or 'L' or 'm'):
+        elif re.match(r'[7,eQtLm]', i) != None:
           e.append(3)
-        elif i == ('D' or 'p' or '1' or 'U' or '8' or '3' or '('):
+        elif re.match(r'[Dp1U83\(]', i) != None:
           e.append(4)
-        elif i == ('c' or 's' or 'f' or 'T' or '.' or 'k' or 'Z' or 'i'):
+        elif re.match(r'[csfT\.kZi]', i) != None:
           e.append(5)
-        elif i == ('M' or 'Y' or 'n' or '5' or '/' or 'v' or 'W'):
+        elif re.match(r'[MYn5/vW]', i) != None:
           e.append(6)
-        elif i == ('K' or 'y' or '!' or 'N' or 'G' or 'u' or '0' or 'V'):
+        elif re.match(r'[Ky!NGu0V]', i) != None:
           e.append(7)
-        elif i == ('O' or '[' or ')' or 'I' or 'l' or 'b' or 'E'):
+        elif re.match(r'[O\[\)IlbE]', i) != None:
           e.append(8)
         else:
           e.append(9)
