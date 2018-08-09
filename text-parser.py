4eb115aa032629a8269c83536f5e4944200f2cc1
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-09 17:44:39 -0400

    added password_entry() skeleton

--- text-parser.py
+++ text-parser.py
@@ -1,8 +1,14 @@
 """This module interprets text from various sources.
 
 Functions:
-  - Recieves input for and verifies a valid password for signing in.
+  - Gets and verifies a valid password for signing in.
   - Reads text from regular files or direct entry and converts to binary.
   - Write's encrypted text to files.
   - Reads and returns matching section from users.txt.
   - Writes usernames to users.txt."""
+
+def password_entry():
+  """This function is called by accounts.signin() or accounts.new_account() to get the password.
+  
+  It requests a password, checks to see if the password is valid, and if so returns the password."""
+  pass
