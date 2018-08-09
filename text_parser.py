6c82f39aaedf39a5448a1770d66007a7800998b8
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-09 18:39:56 -0400

    added user_entry to accounts.py

--- text_parser.py
+++ text_parser.py
@@ -3,7 +3,7 @@
 Functions:
   - Gets and verifies a valid password for signing in.
   - Reads text from regular files or direct entry and converts to binary.
-  - Write's encrypted text to files.
+  - Writes encrypted text to files.
   - Reads and returns matching section from users.txt.
   - Writes usernames to users.txt."""
 
@@ -11,4 +11,10 @@ def password_entry():
   """This function is called by accounts.user_entry() to get the password.
   
   It requests a password, checks to see if the password is valid, and if so returns the password."""
+  pass
+
+def read_users():
+  pass
+
+def write_users():
   pass
