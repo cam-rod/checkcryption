7de7532f0e10297c550af13c13a7d1ff9535dd4e
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-08-18 21:24:15 -0400

    added textcryption.py

---
+++ textcryption.py
@@ -0,0 +1,4 @@
+"""This is the main module of Textcryption.
+
+This module introduces the program to the user and calls other module to carry out specific actions.
+The module also acts a the main menu."""
